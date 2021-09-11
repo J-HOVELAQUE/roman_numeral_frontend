676541046d19d80a03cd2275d1a8e61b834d8788
J-HOVELAQUE <j.hovelaque@orange.fr>
2021-09-11 19:14:11 +0200

    adding state for input value

--- src/component/RomanNumeralsConverter.jsx
+++ src/component/RomanNumeralsConverter.jsx
@@ -1,11 +1,27 @@
-import "./romanNumeralsConverterStyle.css"
+import "./romanNumeralsConverterStyle.css";
+import { useState } from "react";
 
 export default function RomanNumeralsConverter() {
+  const [romanNumeral, setRomanNumeral] = useState("");
+
+  const onSubmitRomanNumeral = (e) => {
+    e.preventDefault();
+    console.log("VALUE",romanNumeral );
+  }
+
   return (
     <div className="roman-numeral-converter">
-      <form className="converter-form">
+      <form
+        className="converter-form"
+        onSubmit={(e) => {onSubmitRomanNumeral(e)}}
+      >
         <label htmlFor="roman-numerals-input" className="converter-label">Entrez le chiffre romain que vous souhaitez convertir</label>
-        <input type="text" id="roman-numerals-input" className="converter-input"/>
+        <input
+          type="text" id="roman-numerals-input"
+          className="converter-input"
+          onChange={(e) => setRomanNumeral(e.target.value)}
+          value={romanNumeral}
+        />
         <button className="converter-button">Convertir</button>
       </form>
       <h3 className="result-zone">XV correspond à 15 en chiffres arabes</h3>
